5c7f80087dcd7962c89ca7fb49e41ad66ef5b819
patrick napper and saneyee sarkar <saneyee@>
2017-11-29 15:08:42 -0800

    logic works

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -1,55 +1,39 @@
 
-
-// import {Bike} from './../js/bike.js';
-
 $(document).ready(function() {
 
+  $('#formOne').submit(function(e) {
+    e.preventDefault();
 
-    $('#formOne').submit(function(e) {
-        e.preventDefault();
+    let location = $('.location').val();
+    $('.location').val("");
 
-        let location = $('.location').val();
-        // let stolenDate = new Date($('.date').val());
+    $.ajax({
+      url: `https://bikeindex.org:443/api/v3/search?location=${location}&distance=60&stolenness=stolen`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        let currentDate = (Date.parse(new Date()))/1000;
+        let pastweekDate = currentDate - (60*60*24*7);
+        response.bikes.forEach(function(item) {
 
-        $('.location').val("");
-        // $('.date').val("");
-
-        // bike = new Bike(stolenDate);
-
-
-        $.ajax({
-            url: `https://bikeindex.org:443/api/v3/search?location=seattle&stolenness=stolen`,
-            type: 'GET',
-            data: {
-                format: 'json'
-            },
-            success: function(response) {
-              let currentDate = Date.parse(new Date());
-              let pastweekDate =   Date.parse(new Date())-(7 * 24 * 60 * 60 * 1000);
-
-                response.bikes.forEach(function(item) {
-                  if (item.date_stolen <= currentDate && item.date_stolen >= pastweekDate) {
-                      $('#output').append(`${item.title}`+"<br>"+`${item.date_stolen}`);
-
-                  }
-
-
-
-              });
-                // if limit > 0;
-
-                // response.data.forEach(function(item){
-                //   $('#pic').append("<img src='"+item.images.fixed_height_still.url+"' />");
-                // });
-            },
-            error: function() {
-                $('#errors').text("There was an error processing your request. Please try again.");
-            }
+          if (item.date_stolen <= currentDate && item.date_stolen >= pastweekDate) {
+            var d = new Date(item.date_stolen*1000)
+            var n = d.toUTCString();
+            $('#output').append(`${item.title}`+"<br>"+`${n}`+"<br>");
+          }
         });
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+    $("#formOne").hide();
+    $("#output").show();
 
-        $("#formOne").hide();
-        $("#output").show();
 
-            });
+
+  });
 
 });
